0efefd9127cbe50f0ce699a770da0dff4261e448
Sanil <sanilrohra7@gmail.com>
2026-08-12 17:51:26 +0530

    vnet second commit

--- main.tf
+++ main.tf
@@ -9,4 +9,7 @@ resource "azurerm_virtual_network" "vNet" {
     resource_group_name = "TCS-TF-Labs"
     name = "vNet-19"
     address_space = [ "10.7.0.0/16" ]
+    tags = {
+        createdby = "Sanil"
+    }
 }
