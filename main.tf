f629f91f2b8de179892d770656144b3350d9c387
Sanil <sanilrohra7@gmail.com>
2026-08-12 17:26:32 +0530

    Azure vNet

--- main.tf
+++ main.tf
@@ -4,7 +4,9 @@ provider "azurerm" {
     }
 }
 
-resource azurerm_virtual_network vnet19 {
+resource "azurerm_virtual_network" "vNet" {
+    location = "CentralIndia"
+    resource_group_name = "TCS-TF-Labs"
     name = "vNet-19"
-    
+    address_space = [ "10.7.0.0/16" ]
 }
